277cc7b1192fae3aab3beb52722a42de937b8009
GRUPPA02a0112 <tr1pple@mail.ru>
2014-10-17 12:20:50 +0400

    в ВЕТВИ ДИВИЖН ДОБАВИЛИ ДЕЛЕНИЕ

--- main.cpp
+++ main.cpp
@@ -7,9 +7,11 @@ int main()
     cout <<"Enter 2 integer numbers:" << endl;
     cin >> a >> b;
     cout << "Your numbers are:" << endl;
-    cout << a << endl << b;
+    cout << a << endl << b << endl;
     cout << "Sum is:" << endl;
     cout << (a+b) << endl;
     cout << "Difference is:" << endl;
     cout << (a-b) << endl;
+    cout << "Division is:" << endl;
+    cout << (a/b) << endl;
 }
